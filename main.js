b0e0765a7fc3dd9c99474f2d1a3c0683ef1a4780
Histman7 <hsandovalt1900@alumno.ipn.mx>
2024-03-01 22:01:22 -0600

    HTML structure

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="location">
+    <h1 class="location-timezone">Timezone</h1>
+    <p>Icon</p>
+  </div>
+  <div class="temperature">
+    <h2 class="temperature-degree">34</h2>
+    <div class="temperature-description">It's frigging cold</div>
+  </div>
+`
+

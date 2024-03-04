9fd5be8db5bc15d5cd53dfbd1fedbc362bb13f70
Histman7 <hsandovalt1900@alumno.ipn.mx>
2024-03-03 21:47:49 -0600

    fin

--- main.js
+++ main.js
@@ -1,13 +1,76 @@
-import './style.css'
+const resultElement = document.querySelector('.result')
+const formElement = document.querySelector('.get-weather')
+const nameCityElement = document.querySelector('#city')
+const nameCountryElement = document.querySelector('#country')
 
-document.querySelector('#app').innerHTML = `
-  <div class="location">
-    <h1 class="location-timezone">Timezone</h1>
-    <p>Icon</p>
-  </div>
-  <div class="temperature">
-    <h2 class="temperature-degree">34</h2>
-    <div class="temperature-description">It's frigging cold</div>
-  </div>
-`
+formElement.addEventListener('submit', e => {
+  e.preventDefault()
+  
+  if (nameCityElement.value === '' || nameCountryElement.value === ''){
+    showError('Ambos campos son obligatorios')
+  }
+  
+  callAPI(nameCityElement.value, nameCountryElement.value)
+  
+  // console.log(nameCityElement.value)
+  // console.log(nameCountryElement.value)
+})
 
+function showError(message) {
+  console.log(message)
+  const alert = document.createElement('p')
+  alert.classList.add('alert-message')
+  alert.textContent = message
+  formElement.appendChild(alert)
+  
+  setTimeout(()=>{
+    alert.remove()
+  }, 3000)
+}
+
+function callAPI(city, country) {
+  const apiKey = 'a5e10f274c240f286b0af15cb52a8a4c'
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`
+  
+  fetch(url)
+      .then(response=>response.json())
+      .then(data=>{
+        
+        clearHTML()
+        if (data?.cod === '404') showError('Ciudad no encontrada')
+        else showWeather(data)
+        
+        console.log(data)
+      })
+      .catch(error=>{
+        console.log(error)
+      })
+}
+
+function showWeather(data) {
+  const {name, main:{temp, temp_min, temp_max}, weather:[arr]} = data
+  const contentElement = document.createElement('div')
+  const degree = kelvinToCelcius(temp)
+  const degreeMax = kelvinToCelcius(temp_max)
+  const degreeMin = kelvinToCelcius(temp_min)
+  
+  contentElement.innerHTML = `
+      <h5>Clima en ${name}</h5>
+      <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
+      <h2>${degree}°C</h2>
+      <p>Max: ${degreeMax}°C</p>
+      <p>Min: ${degreeMax}°C</p>
+  `
+  
+  resultElement.appendChild(contentElement)
+  
+  console.log(name, temp, temp_min, temp_max, arr?.icon)
+}
+
+function kelvinToCelcius(grades) {
+  return parseInt(grades - 273.15)
+}
+
+function clearHTML() {
+  resultElement.innerHTML = ''
+}
